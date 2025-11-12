27c1d68eb7ab944aaa9104a671cec6c633a7605b
Augusto Valerio <augusto.valerio06@gmail.com>
2025-11-11 21:24:35 -0300

    Feat: adicionar CardUser a pagina de ModalUser

--- src/pages/ModalUser.jsx
+++ src/pages/ModalUser.jsx
@@ -1,9 +1,23 @@
-import { useParams } from "react-router-dom"
-export function ModalUser(){
-    const {user} = useParams()
-    return(
-        <>
-            <h1>modal! de {user}</h1>
-        </>
-    )
+import { useEffect } from "react";
+import { useState } from "react";
+
+import { CardUser } from "@/components/CardUser";
+import { useParams } from "react-router-dom";
+export function ModalUser() {
+  const { user } = useParams();
+
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://69128e0c52a60f10c821f8f7.mockapi.io/connect/users")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+
+  return (
+    <>
+      <section>
+        <CardUser />
+      </section>
+    </>
+  );
 }
