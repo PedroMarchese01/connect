a2f2b50d0bf4b74e5536a9d8e3b1a54ccee706df
Augusto Valerio <augusto.valerio06@gmail.com>
2025-11-12 19:39:55 -0300

    Feat: adicionar rota dinâmica para a pagina de cada usuário

--- src/pages/ModalUser.jsx
+++ src/pages/ModalUser.jsx
@@ -2,21 +2,15 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 import { CardUser } from "@/components/CardUser";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 export function ModalUser() {
   const { user } = useParams();
-
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    fetch("https://69128e0c52a60f10c821f8f7.mockapi.io/connect/users")
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  }, []);
 
   return (
     <>
       <section>
-        <CardUser />
+        <Link to="/">Voltar</Link>
+        <h1>Modal {user}</h1>
       </section>
     </>
   );
