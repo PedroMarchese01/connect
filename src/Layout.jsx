eb284e8e473bffac6931da9763a436fd6d705dcb
Augusto Valerio <augusto.valerio06@gmail.com>
2025-11-12 21:55:27 -0300

    Fix: arrumar background do layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,15 +4,13 @@ import Navbar from "./components/Navbar";
 export default function Layout() {
   return (
     <>
-      <div className="bg-[#f9f9f9] overflow-x-hidden ">
-        <nav className="w-full h-full ">
-          <Navbar />
-        </nav>
-        <main className="">
-          <Outlet />
-        </main>
-        <footer></footer>
-      </div>
+      <nav>
+        <Navbar />
+      </nav>
+      <main>
+        <Outlet />
+      </main>
+      <footer></footer>
     </>
   );
 }
