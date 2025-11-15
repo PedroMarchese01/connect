2359bf4fc09ed7eca32ada8e8cff29acf23cd301
Augusto Valerio <augusto.valerio06@gmail.com>
2025-11-15 01:05:02 -0300

    Feat: instalar componentes toaster, modal e textarea do shadcn

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
+import { Toaster } from "@/components/ui/sonner";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <>
     <RouterProvider router = {router}/>
+    <Toaster position="top-center" /> 
+    </>
   </StrictMode>,
 )
